7fd6acc561163f4eff932cc623f81c889b15aed4
Thomas Bonavia <thomas.bonavia@apave.com>
2017-02-10 13:00:37 +0100

    Paramétrage du build client en fonction de la plateforme

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -7,7 +7,21 @@ var gulp = require('gulp');
 // Importe l'ensemble des tâches Gulp configurées individuellement
 requireDir('./gulp/tasks', {recurse: true});
 
-// La tâche par défaut à exécuter.
-gulp.task('default', [], function(){
-  console.log('================================== HELLO !!!');
+// Paramétrer les tâches à réaliser pour construire l'application dans chacun des
+// environnement => Par ex. pas de minification du code en dev mais minification
+// en test et prod !
+var tasksDev = [];
+var tasksTest = [];
+var tasksProd = [];
+
+gulp.task('dev', tasksDev, function(){
+  console.log('================================== HELLO DEV !!!');
+});
+
+gulp.task('test', tasksTest, function(){
+  console.log('================================== HELLO TEST !!!');
+});
+
+gulp.task('prod', tasksProd, function(){
+  console.log('================================== HELLO PROD !!!');
 });
