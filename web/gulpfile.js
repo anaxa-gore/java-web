1a1164bd4a20aa8c6605f04509b1db660fa46b29
Thomas Bonavia <thomas.bonavia@apave.com>
2017-02-10 10:36:31 +0100

    Build de l'application WEB avec Maven

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -2,6 +2,12 @@
 'use strict';
 
 var requireDir = require('require-dir');
+var gulp = require('gulp');
 
 // Importe l'ensemble des tâches Gulp configurées individuellement
 requireDir('./gulp/tasks', {recurse: true});
+
+// La tâche par défaut à exécuter.
+gulp.task('default', [], function(){
+  console.log('================================== HELLO !!!');
+});
